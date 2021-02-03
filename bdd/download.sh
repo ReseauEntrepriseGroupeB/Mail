0f6a19c4875a0f0e4bb9db00f7a025977716adfa
CastermaneRobin <robin@castermane.be>
2021-02-03 12:31:53 +0100

    Create download.sh

---
+++ bdd/download.sh
@@ -0,0 +1,5 @@
+curl -o docker-compose.yml https://github.com/ReseauEntrepriseGroupeB/Mail/blob/main/bdd/docker-compose.yml
+mkdir sql-scripts
+curl -o sql-scripts/db-init.sql https://github.com/ReseauEntrepriseGroupeB/Mail/blob/main/bdd/sql-scripts/db-init.sql
+
+
