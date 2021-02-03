30b6db4534acc7270b3ddc0c6040c21b5c925835
Robin <robin@castermane.be>
2021-02-03 12:12:19 +0100

    Update db-init.sql

--- bdd/sql-scripts/db-init.sql
+++ bdd/sql-scripts/db-init.sql
@@ -25,18 +25,12 @@ CREATE TABLE `virtual_aliases` (
 
 
 
-INSERT INTO `mailserver`.`virtual_domains`
-  (`name`)
-VALUES
-  ('ephec-ti.eu'),('mail.ephec-ti.eu'),('mail'),('localhost.ephec-ti.eu');
+INSERT INTO `db`.`virtual_domains` (`name`) VALUES ('ephec-ti.eu'),('mail.ephec-ti.eu'),('mail'),('localhost.ephec-ti.eu');
 
-INSERT INTO `mailserver`.`virtual_users`
-  (`domain_id`, `password` , `email`)
-VALUES
-  (1, 'user1234', 'root@ephec-ti.eu'), (1, 'user1234', 'contact@ephec-ti.eu'), (1, 'user1234', 'directeur@ephec-ti.eu'), (1, 'user1234', 'b2b@ephec-ti.eu'), (1, 'user1234', 'florent@ephec-ti.eu');
+INSERT INTO `db`.`virtual_users` (`domain_id`, `password` , `email`) VALUES ('1', 'user1234', 'root@ephec-ti.eu'), ('1', 'user1234', 'contact@ephec-ti.eu'), ('1', 'user1234', 'directeur@ephec-ti.eu'), ('1', 'user1234', 'b2b@ephec-ti.eu'), ('1', 'user1234', 'florent@ephec-ti.eu');
 
 
-INSERT INTO `mailserver`.`virtual_aliases`
-  (`domain_id`, `source`, `destination`)
-VALUES
-  (1, 'florent@ephec-ti.eu', 'florent@ephec-ti.eu');
+INSERT INTO `db`.`virtual_aliases` (`domain_id`, `source`, `destination`) VALUES ('1', 'florent@ephec-ti.eu', 'florent@ephec-ti.eu');
+
+
+
