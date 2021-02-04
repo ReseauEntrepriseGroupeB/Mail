499b54e383e6ceeecf59c16d12e956b3eb4d358b
Florent <55687537+Frodon001@users.noreply.github.com>
2021-02-04 10:01:18 +0100

    Update db-init.sql

--- bdd/sql-scripts/db-init.sql
+++ bdd/sql-scripts/db-init.sql
@@ -25,12 +25,12 @@ CREATE TABLE `virtual_aliases` (
 
 
 
-INSERT INTO `mailserver`.`virtual_domains` (`name`) VALUES ('ephec-ti.eu'),('mail.ephec-ti.eu'),('mail'),('test.ephec-ti.eu');
+INSERT INTO `mailserver`.`virtual_domains` (`name`) VALUES ('ephec-ti.eu'),('intra.ephec-ti.eu')('mail.ephec-ti.eu'),('mail'),('test.ephec-ti.eu');
 
 INSERT INTO `mailserver`.`virtual_users` (`domain_id`, `password` , `email`) VALUES ('1', 'user1234', 'root@ephec-ti.eu'), ('1', 'user1234', 'contact@ephec-ti.eu'), ('1', 'user1234', 'directeur@ephec-ti.eu'), ('1', 'user1234', 'b2b@ephec-ti.eu'), ('1', 'user1234', 'florent@ephec-ti.eu');
 
 
-INSERT INTO `mailserver`.`virtual_aliases` (`domain_id`, `source`, `destination`) VALUES ('1', 'florent@ephec-ti.eu', 'florent@ephec-ti.eu');
+INSERT INTO `mailserver`.`virtual_aliases` (`domain_id`, `source`, `destination`) VALUES ('1', 'florent@ephec-ti.eu', 'florent@ephec-ti.eu'),('1', 'Igor@ephec-ti.eu', 'Igor@ephec-ti.eu');
 
 
 
